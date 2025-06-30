7e11926b6cddea242c49a59bbc0e170bf77a201c
rparekh01 <rajparekh001@outlook.com>
2025-06-29 19:49:41 -0500

    hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,23 @@
 export default function Hero() {
   return (
-    <section id="hero">
-      <div>
-        Hello, My name is
-        <div>Raj Parekh</div>
-        <div>I like to build web apps.</div>
-        <div>
+    <section
+      id="hero"
+      className="min-h-screen bg-slate-900 text-white flex items-center"
+    >
+      <div className="container mx-auto px-6">
+        <p className="text-teal-400 font-mono text-sm mb-4">Hi, My name is</p>
+        <h1 className="text-5xl md:text-7xl font-bold text-slate-200 mb-4">
+          Raj Parekh
+        </h1>
+        <h2 className="text-4xl md:text-6xl font-bold text-slate-400 mb-6">
+          I build web applications.
+        </h2>
+        <p className="text-slate-200 text-lg mb-12 leading-relaxed max-w-2xl">
           I&apos;m full-stack software engineer specializing in building
           exception digital experinces. Currently, I&aposm focusing on building
           a highly personlized sales application for insurance agents at
           MagMutual Insurance Company.
-        </div>
+        </p>
       </div>
     </section>
   );
