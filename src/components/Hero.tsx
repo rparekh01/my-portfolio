5c7383b9b92aaa691b6b0c431f4d9e1f435503ab
rparekh01 <rajparekh001@outlook.com>
2025-07-12 08:37:37 -0400

    reduce fluff

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,10 +6,8 @@ export default function Hero() {
         <h1 className="text-5xl md:text-7xl font-bold text-slate-200 mb-4">
           Raj Parekh
         </h1>
-        <h2 className="text-4xl md:text-6xl font-bold text-slate-400 mb-6">
-          I bring ideas to life on the web.
-        </h2>
-        <p className="text-slate-200 text-lg mb-12 leading-relaxed max-w-2xl">
+
+        <p className="text-slate-400 text-lg mb-12 leading-relaxed max-w-2xl">
           I&apos;m a full-stack software engineer specializing in building
           scalable, high-performance enterprise applications that deliver great
           user experiences.
